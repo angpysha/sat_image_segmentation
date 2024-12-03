13db41e56d0971393152c689b2476373c6e297fe
Andrii Petrovskyi <angpysha@outlook.com>
2024-12-03 21:25:15 +0200

    Run code fixes

--- layers/naive_bayes.py
+++ layers/naive_bayes.py
@@ -1,6 +1,7 @@
 from keras.layers import Layer
 import numpy as np
-from keras import backend as K
+from tensorflow.keras.backend import *
+import tensorflow as tf
 
 class NaiveBayesLayer(Layer):
     def __init__(self, num_features, num_of_categories, **kwargs):
@@ -22,14 +23,14 @@ class NaiveBayesLayer(Layer):
 
     def call(self, X):
         # Gaussian PDF for each class
-        probs = [K.prod(self.gaussian_pdf(X, self.means[i], self.variances[i]), axis=1) for i in range(self.num_of_categories)]
-        probs_tensor = K.stack(probs, axis=1)
+        probs = [prod(self.gaussian_pdf(X, self.means[i], self.variances[i]), axis=1) for i in range(self.num_of_categories)]
+        probs_tensor = stack(probs, axis=1)
         # Normalizing to get the posterior probabilities for each class
-        probs_normalized = probs_tensor / K.sum(probs_tensor, axis=1, keepdims=True)
+        probs_normalized = probs_tensor / sum(probs_tensor, axis=1, keepdims=True)
         return probs_normalized
 
     def gaussian_pdf(self, X, mean, variance):
-        return K.exp(-K.square(X - mean) / (2 * variance)) / K.sqrt(2 * np.pi * variance)
+        return exp(-square(X - mean) / (2 * variance)) / sqrt(2 * np.pi * variance)
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], self.num_of_categories)
@@ -55,12 +56,21 @@ class MultiClassNaiveBayesLayer(Layer):
 
     def call(self, X):
         # Gaussian PDF for each class
-        prob_list = [K.prod(self.gaussian_pdf(X, self.means[i], self.variances[i]), axis=1) for i in range(self.num_of_categories)]
-        probs = K.stack(prob_list, axis=-1)
+        prob_list = [prod(self.gaussian_pdf(X, self.means[i], self.variances[i]), axis=1) for i in range(self.num_of_categories)]
+        probs = stack(prob_list, axis=-1)
         return probs
 
+    # def gaussian_pdf(self, X, mean, variance):
+    #     return K.exp(-K.square(X - mean) / (2 * variance)) / K.sqrt(2 * np.pi * variance)
     def gaussian_pdf(self, X, mean, variance):
-        return K.exp(-K.square(X - mean) / (2 * variance)) / K.sqrt(2 * np.pi * variance)
+        # Safe division and exponentiation
+        epsilon = 1e-9  # Small constant to avoid division by zero
+        variance_safe = tf.where(tf.equal(variance, 0), tf.ones_like(variance) * epsilon, variance)
+        pdf = exp(-square(X - mean) / (2 * variance_safe)) / sqrt(2 * np.pi * variance_safe)
+
+        # Handle NaN values by replacing them with 0
+        pdf = tf.where(tf.math.is_nan(pdf), tf.zeros_like(pdf), pdf)
+        return pdf
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], self.num_of_categories)
